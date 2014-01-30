67ec95316060a2c650e7bf645c013d85ccd54c0c
RoxanaIoana <roxana.ioana92@gmail.com>
2014-01-31 00:18:46 +0200

    log

---
+++ GestiuneaSalilor/GestiuneaSalilor/BusinessLayerClass.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+
+
+namespace GestiuneaSalilor
+{
+    class BusinessLayerClass
+    {
+
+        public DataLayerClass dlc = new DataLayerClass();
+        Users us = new Users();
+
+
+        public string DecryptPassword(string encryptedPassword)
+        {
+            byte[] passByteData = Convert.FromBase64String(encryptedPassword);
+            string originalPassword = System.Text.Encoding.Unicode.GetString(passByteData);
+            return originalPassword;
+        }
+
+        public Users SetUs(int i, string a, string b)
+        {
+            us = dlc.SetUser(i, a, b);
+
+            return us;
+        }
+
+
+        public bool InsertUs()
+        {
+            if (dlc.InsertUser())
+                return true;
+            else 
+                return false;
+        }
+
+
+        public bool SearchUser(string name)
+        {
+            
+                    DataTable dt = new DataTable();
+
+                    dt = dlc.SelectU();
+                    int k = 0;
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        if (dr["Username"].ToString() == name)
+                            k = 1;
+
+                    }
+
+                    if (k == 1)
+                        return true;
+                    else
+                        return false;
+
+                    
+
+
+        }
+
+
+        public string ReturnPassword(string name)
+        {
+                     DataTable dt = new DataTable();
+
+                    dt = dlc.SelectU();
+                    int k = 0;
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        if (dr["Username"].ToString() == name)
+                            return dr["Password"].ToString();
+                        k = 1;
+                    }
+
+                
+                    return null;
+
+                  
+
+        }
+
+
+
+        public DataTable SelectSalaa()
+        {
+            DataTable dt = new DataTable();
+            dt = dlc.SelectSala();
+            return dt;
+
+
+         }
+
+        public DataTable SelectInvt()
+        {
+            DataTable dt = new DataTable();
+            dt = dlc.SelectInventar();
+            return dt;
+
+
+        }
+
+
+    }
+}
---
+++ GestiuneaSalilor/GestiuneaSalilor/Log.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace GestiuneaSalilor
+{
+    partial class Log
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Log));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 237);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Login as guest";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Gestiunea salilor de clasa";
+//            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(115, 129);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 24);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Administrator";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Log
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(326, 302);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Log";
+            this.Text = "Log";
+            this.Load += new System.EventHandler(this.Log_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+
+    }
+}
