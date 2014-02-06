a3bcf19067885ab26aaa46af454d5a2a1229f8e0
slv92 <silviu.gabriel92@gmail.com>
2014-02-06 14:24:22 +0200

    1

--- GestiuneaSalilor/GestiuneaSalilor/Properties/Settings.Designer.cs
+++ GestiuneaSalilor/GestiuneaSalilor/Properties/Settings.Designer.cs
@@ -32,5 +32,16 @@ namespace GestiuneaSalilor.Properties {
                 return ((string)(this["Sali"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\v11.0;AttachDbFilename=|DataDirectory|\\Sali.mdf;Integrated " +
+            "Security=True")]
+        public string SaliConnectionString {
+            get {
+                return ((string)(this["SaliConnectionString"]));
+            }
+        }
     }
 }
